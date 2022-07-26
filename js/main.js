1d27afca5c6b20c9dae79ef54f0db3f62586513e
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-25 19:45:08 -0500

    Create Desktop and movile version in Home page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 /* eslint-disable linebreak-style */
 const burgerButton = document.querySelector('.menu-button');
 const burgerBars = document.querySelector('.menu-button span');
+const menuModal = document.querySelector('#menuModal');
+const menuList = document.querySelector('#menuModalList');
+
+menuList.addEventListener('click', () => {
+  burgerBars.classList.toggle('open');
+  menuModal.classList.toggle('open');
+});
 
 burgerButton.addEventListener('click', () => {
   burgerBars.classList.toggle('open');
+  menuModal.classList.toggle('open');
 });
