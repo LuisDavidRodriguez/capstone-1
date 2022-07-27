336dc363ef9520e0048e0a9dd917c673a0f5cdc4
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-26 19:57:08 -0500

    Solve images link javaScript, correct linters errors

--- js/program.js
+++ js/program.js
@@ -1,27 +1,27 @@
 const programContainer = document.querySelector('#programContainer');
 const programData = [
   {
-    image: '../images/icons/icon_presenter.png',
+    image: 'images/icons/icon_presenter.png',
     title: 'Lecture',
     description: 'Listen to speakers from varius countries about messeges of sharing and opening.',
   },
   {
-    image: '../images/icons/icon_program.png',
+    image: 'images/icons/icon_program.png',
     title: 'CC Exhibition',
     description: 'Appreciate various creations applying CC license of artists, organized from Art Center Nabi',
   },
   {
-    image: '../images/icons/icon_messages.png',
+    image: 'images/icons/icon_messages.png',
     title: 'Forum',
     description: 'Have the time to share your thoughts and opinions with experts for each topic.',
   },
   {
-    image: '../images/icons/icon_board.png',
+    image: 'images/icons/icon_board.png',
     title: 'Workshop',
     description: 'Try creating your own work using open source license rather than just watching.',
   },
   {
-    image: '../images/icons/icon_wheel.png',
+    image: 'images/icons/icon_wheel.png',
     title: 'CC Ignite',
     description: 'Get opportunities to network with CC affiliates around the world, also after the summit.',
   },
@@ -48,6 +48,5 @@ export default function populateProgram (arr = programData) {
 
     programContainer.appendChild(card);
   });
-  
 
 }
