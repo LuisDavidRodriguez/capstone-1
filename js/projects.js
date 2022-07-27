9c4cf3d10628fec64acaa38c934f38f2c4874209
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-27 16:30:31 -0500

    Change images and stars

--- js/projects.js
+++ js/projects.js
@@ -1,74 +1,82 @@
 const speakersContainer = document.querySelector('#speakersContainer');
+const showMoreBtn = document.querySelector('#showMoreSpeakers');
+const hideMoreBtn = document.querySelector('#hideMoreSpeakers');
 const speakersData = [
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 1',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 2',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 3',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 4',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 5',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 6',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 7',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 8',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 9',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 10',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 11',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
   {
     image: 'images/presenters/speaker_01.png',
-    name: 'Yoachi Benkeler',
+    name: 'Yoachi Benkeler 12',
+    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
+    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+  },
+  {
+    image: 'images/presenters/speaker_01.png',
+    name: 'Yoachi Benkeler 13',
     carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
     expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
   },
@@ -76,6 +84,8 @@ const speakersData = [
 
 export default function populateSpeakers (arr = speakersData) {
 
+  const arrCards = [];
+  let index = 0;
   arr.forEach((speaker) => {
     const card = document.createElement('div');
     const image = document.createElement('img');
@@ -85,7 +95,9 @@ export default function populateSpeakers (arr = speakersData) {
     const divition = document.createElement('hr');
     const expirience = document.createElement('p');
 
+    card.id = `speaker${index}`;
     card.classList.add('card-speakers', 'col-12-base', 'col-12-desktop', 'col-5-xlarge');
+    card.classList.add('invisible');
     image.classList.add('card-img');
     cardBody.classList.add('card-body');
     title.classList.add('card-title');
@@ -100,9 +112,19 @@ export default function populateSpeakers (arr = speakersData) {
 
     cardBody.append(title, carier, divition, expirience);
     card.append(image, cardBody);
-
-    speakersContainer.appendChild(card);
+    speakersContainer.append(card);
+    index++;
   });
-  
 
 }
+
+
+
+
+
+
+
+
+
+
+
