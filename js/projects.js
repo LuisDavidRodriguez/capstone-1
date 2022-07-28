e93e97c9fde750d71536ab03aef55ae363a3c18a
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-28 00:33:38 -0500

    Add animation to the cards

--- js/projects.js
+++ js/projects.js
@@ -7,126 +7,125 @@ const hideMoreBtn = document.querySelector('#hideSpeakers');
 const speakersData = [
   {
     image: 'images/presenters/1.png',
-    name: 'Yoachi Benkeler 1',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Nahomi Perez 1',
+    carier: 'English teacher, brazilian straightening',
+    expirience: 'My experience with straightening was really incredible',
   },
   {
     image: 'images/presenters/2.png',
-    name: 'Yoachi Benkeler 2',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Yovanna Gutierrez 2',
+    carier: 'Private company secretary,organic straightening ',
+    expirience: 'I fell in love with my hair,I cant believe it turned out so perfect, thank you'
   },
   {
     image: 'images/presenters/3.png',
-    name: 'Yoachi Benkeler 3',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Marlene Sanchez 3',
+    carier: 'Gastronomy student,straightening shine gold',
+    expirience: 'I super super recommend it, they leave your hair super straight',
   },
   {
     image: 'images/presenters/4.png',
-    name: 'Yoachi Benkeler 4',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Matha Herrera 4',
+    carier: 'Primary math teacher, organic smoothing',
+    expirience: 'Happy super happy attend the expo you will not regret it',
   },
   {
     image: 'images/presenters/5.png',
-    name: 'Yoachi Benkeler 5',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Lorena Mola 5',
+    carier: 'Secondary physical education teacher, silk gloss straightening',
+    expirience: 'My hair is left without frizz, without volume',
   },
   {
     image: 'images/presenters/6.png',
-    name: 'Yoachi Benkeler 6',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Yara Ortiz 6',
+    carier: 'Secondary physical education teacher, argan hair mask',
+    expirience: 'I love this organic brand, it really is perfect',
   },
   {
     image: 'images/presenters/7.png',
-    name: 'Yoachi Benkeler 7',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Beatriz Villalobos 7',
+    carier: 'Chemical engineering student,hair growth shampoo',
+    expirience: 'I had used several brands to grow my hair and had not been successful until I met yaz fortozo my hair has grown quite a bit',
   },
   {
     image: 'images/presenters/8.png',
-    name: 'Yoachi Benkeler 8',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Yara Marquez 8',
+    carier: 'Communication, organic straightening',
+    expirience: 'My hair really needed help, im happy',
   },
   {
     image: 'images/presenters/9.png',
-    name: 'Yoachi Benkeler 9',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Lucila beffman 9',
+    carier: 'Degree in natural sciences, smoothing chocolate',
+    expirience: 'Grateful for the treatment, explanation, and attention, my hair was incredible',
   },
   {
     image: 'images/presenters/10.png',
-    name: 'Yoachi Benkeler 10',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Lara Rodriguez 10',
+    carier: 'Cosmetologist, chocolate hair mask',
+    expirience: 'After bleaching, this mask has hydrated my hair, I recommend the brand, go to the exhibition',
   },
   {
     image: 'images/presenters/11.png',
-    name: 'Yoachi Benkeler 11',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Gloria Vazquez 11',
+    carier: 'International trade, organic straightening',
+    expirience: 'After wasting so much time ironing my hair every day, wasting time and money, knowing this straightening has been the best',
   },
   {
     image: 'images/presenters/12.png',
-    name: 'Yoachi Benkeler 12',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Yug Cortez 12',
+    carier: 'Political Science, Argan Hair Mask',
+    expirience: 'It has hydrated my hair a lot, it is rich and not greasy, the best thing is that it is organic, I love that',
   },
   {
     image: 'images/presenters/13.png',
-    name: 'Yoachi Benkeler 13',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Dominica Tellez 13',
+    carier: 'Housewife, Brazilian Straightening',
+    expirience: 'It has been an excellent option for me, I highly recommend it.',
   },
   {
     image: 'images/presenters/14.png',
-    name: 'Yoachi Benkeler 14',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Sonia Colkin 14',
+    carier: 'Commercial manager, organic straightening',
+    expirience: 'If you want to save time, this option is ideal for you, you will no longer have to spend time ironing your hair',
   },
   {
     image: 'images/presenters/15.png',
-    name: 'Yoachi Benkeler 15',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Julia Monroe 15',
+    carier: 'Student in nutrition, organic straightening',
+    expirience: 'To you who will read this, I highly recommend it, it does not mistreat the hair, on the contrary, your hair is super soft',
   },
   {
     image: 'images/presenters/16.png',
-    name: 'Yoachi Benkeler 16',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Vianey Pineda 16',
+    carier: 'Public accountant, japanese smoothing',
+    expirience: 'I am delighted that I have not felt discomfort when applying this straightening, I recommend it',
   },
   {
     image: 'images/presenters/17.png',
-    name: 'Yoachi Benkeler 17',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Sandra Peralta 17',
+    carier: 'Language teacher, smooth shampoo',
+    expirience: 'If you want to continue keeping your hair straight, I recommend this shampoo, it leaves it incredible without frizz and hydrated',
   },
   {
     image: 'images/presenters/18.png',
-    name: 'Yoachi Benkeler 18',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Mayra Fuentes 18',
+    carier: 'Student in business administration, organic straightening and maintenance kit',
+    expirience: 'Happy with the collection, they gave me a kit and it is perfect to keep my hair straight',
   },
   {
     image: 'images/presenters/19.png',
-    name: 'Yoachi Benkeler 19',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Dennise Nuñez 19',
+    carier: 'Public accountant, organic straightening',
+    expirience: 'This straightening is not uncomfortable like others, and it leaves the hair super beautiful, I recommend it',
   },
   {
     image: 'images/presenters/20.png',
-    name: 'Yoachi Benkeler 20',
-    carier: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard law School',
-    expirience: 'Benkeler studies commons-based peer production, and plbleshed hes seminal book The Wealth of Networks in 2006',
+    name: 'Brenda Valderas 20',
+    carier: 'Diving, organic straightening and argan hair mask',
+    expirience: 'With my activity I had horrible hair, these products have helped me a lot to restore it, I recommend them go to the expo',
   },
 ];
-
 class CardsManager {
   constructor(parentContainer) {
     this.cards = [];
@@ -170,12 +169,24 @@ class CardsManager {
         this.itemsRemain = 0;
         break;
       }
+      // when the card is added to the parent now is displayed add a transition to disappear the card
+      this.cards[i].classList.add('translate');
       this.parentContainer.append(this.cards[i]);
     }
 
+    // calculate again the items remain and items displayed to always have the newest values
     this.setItemsDisplayed();
-    // how many items are left to be displayed
     this.setItemsRemain();
+
+    // remove the transition you must give a little delay once the card is displayed
+    // the card is hidden once the timer goes of it remove the translate class
+    // and the cards are shown with a little and nice effect
+    setTimeout(() => {
+      const item = this.parentContainer.querySelectorAll('.card-speakers');
+      item.forEach((element) => {
+        element.classList.remove('translate');
+      });
+    }, 200);
   }
 
   removeCards() {
@@ -200,7 +211,6 @@ function createCards(arr = speakersData) {
 
     card.id = `speaker${index}`;
     card.classList.add('card-speakers', 'col-12-base', 'col-12-desktop', 'col-5-xlarge');
-    card.classList.add('invisible');
     image.classList.add('card-img');
     cardBody.classList.add('card-body');
     title.classList.add('card-title');
